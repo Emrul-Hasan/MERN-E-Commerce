eea0e1f3587d6a84976b18504f1475f38edadaaf
Emrul Hasan Emon <imrulhasan003@gmail.com>
2024-11-12 15:39:22 +0600

    update

--- frontend/src/pages/shop/productDetails/SingleProduct.jsx
+++ frontend/src/pages/shop/productDetails/SingleProduct.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 const SingleProduct = () => {
+    const {id} = useParams();
+    console.log(id);
   return (
-    <div>
- 
-    </div>
+<>
+<section className='section__container bg-primary-light'> 
+   <h2 className='section__header capitalize'>Single Product Page</h2>
+   <div className='section__subheader space-x-2'>
+    <span className='hover:text-primary'><Link to="/">home</Link></span>
+    <i className='ri-arrow-right-s-line'></i>
+    <span className='hover:text-primary'><Link to="/shop">shop</Link></span>
+    <i className='ri-arrow-right-s-line'></i>
+    <span className='hover:text-primary'><Link to="/shop">product name</Link></span>
+    <i className='ri-arrow-right-s-line'></i>
+
+   </div>
+   </section>
+</>
   )
 }
 
