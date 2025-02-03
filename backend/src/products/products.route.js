d2b66cc6dfd34b66cde07101708e708da1ecd1f8
Emrul Hasan Emon <imrulhasan003@gmail.com>
2025-02-03 12:50:31 +0600

    admin middleware

--- backend/src/products/products.route.js
+++ backend/src/products/products.route.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const Products = require('./products.model');
 const Reviews = require('../reviews.model');
 const verifyToken = require('../middleware/verifyToken');
+const verifyAdmin = require('../middleware/verifyAdmin');
 const router = express.Router();
 
 router.post("/create-product", async(req, res) =>{
@@ -87,7 +88,7 @@ router.get("/:id", async ( req, res) => {
     }
 })
 //updata a product
-router.patch("/update-product/:id", verifyToken , async (req, res) =>{
+router.patch("/update-product/:id", verifyToken , verifyAdmin, async (req, res) =>{
     try {
         const productId = req.params.id;
         const updateProduct =  await Products.findByIdAndUpdate(productId, {...req.body},{new: true})
